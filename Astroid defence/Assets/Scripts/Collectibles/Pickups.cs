8e87502ba20e2edcc6154d2c138de026230aa160
bconfessor <brian_confessor@poli.ufrj.br>
2016-02-21 15:07:50 +0000

    something

---
+++ Astroid defence/Assets/Scripts/Collectibles/Pickups.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pickups : MonoBehaviour {
+
+	public float minDistance;
+	GameObject player;
+
+
+	// Use this for initialization
+	void Start () {
+		minDistance = 3.0f;
+		player = GameObject.Find ("PlayersShip");//leave me alone Bash, idk how to do it better yet
+	}
+
+	void checkDistance()
+	{
+		//if the player is close enough, move towards them
+		if (Vector3.Distance (transform.position, player.transform.position) < minDistance) {
+		}
+			
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
